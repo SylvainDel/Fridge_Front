061279cb61a0029f6f1447483e0bbbd25f9944b4
SylvainDel <s.delmote@protonmail.com>
2022-08-23 17:59:21 +0200

    Modele Vue Daisy

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold underline mb-10">Hello Vue!</h1>
+    <button class="btn btn-primary">Hello Daisy button</button>
+
+</template>
+
+<style scoped>
+
+</style>
